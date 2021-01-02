7d0cf3af9d748bdef0fbeef1ddac68034952a14e
Litian Zhang <litian_zhang17@163.com>
2021-01-01 14:15:55 -1000

    Refactor code: request info for students dialog

--- lib/Universals.dart
+++ lib/Universals.dart
@@ -1,7 +1,9 @@
 import 'dart:ui';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:virtual_approval_flutter/Request.dart';
 
 class Universals {
   static Color appBarColor = Color(0xff2b9ed4);
@@ -34,6 +36,162 @@ class Universals {
         textColor: Colors.white,
         fontSize: 16.0
     );
+  }
+
+  static showRequestInfoToStudentInRealTime(String email, BuildContext context) {
+
+    showGeneralDialog(
+      context: context,
+      // barrierColor: Colors.black12.withOpacity(0.6), // background color
+      barrierDismissible: false, // should dialog be dismissed when tapped outside
+      barrierLabel: "Dialog", // label for barrier
+      transitionDuration: Duration(milliseconds: 400), // how long it takes to popup dialog after button click
+      pageBuilder: (_, __, ___) { // your widget implementation
+        return SizedBox.expand( // makes widget fullscreen
+          child:
+          StreamBuilder<QuerySnapshot>(
+              stream: FirebaseFirestore.instance
+                  .collection('new requests')
+                  .where('email', isEqualTo: email)
+                  .snapshots(),
+              builder: (context, snapshot){
+                var requestInfo = Column(children: [Text("HI")],);
+                if(snapshot.hasData){
+                  // final content = snapshot.data.documents[0];
+                  print(snapshot.data.docs.length);
+                  final content = snapshot.data.documents[0];
+                  var name = content["name"];
+                  var email = content["email"];
+                  var course = content["course"];
+                  var question = content["question"];
+                  var requestedTimeHawaii = content["requested time hawaii"];
+                  var status = content["status"];
+                  Request request = new Request(name: name, email: email, course: course, question: question, requestedTimeHawaii: requestedTimeHawaii, status: status);
+
+
+                  requestInfo =
+                      Column(
+                        children: <Widget>[
+                          Expanded(
+                            flex: 3,
+                            child: SizedBox.expand(
+                              child: RaisedButton(
+                                color: Universals.transparentColorWhite,
+                                child: Text(
+                                  "Waiting",
+                                  style: TextStyle(fontSize: 40),
+                                ),
+                                textColor: Colors.white,
+                                onPressed: () {
+                                  // Navigator.pop(context);
+                                },
+                              ),
+                            ),
+                          ),
+
+                          Expanded(
+                            flex: 12,
+                            child: SizedBox.expand(
+                              child: RaisedButton(
+                                color: Universals.transparentColorWhite,
+                                child: ListView(
+                                  children: [
+                                    Padding(
+                                      padding: const EdgeInsets.all(5.0),
+                                      child: Center(child: Text(request.name),),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.all(5.0),
+                                      child:  Center(child: Text(request.email),),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.all(5.0),
+                                      child: Center(child: Text(request.course),),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.all(5.0),
+                                      child: Center(child: Text("requested at " + request.requestedTimeHawaii.toString().substring(0, 16)),),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.all(5.0),
+                                      child: Center(child:
+                                      Text(
+                                        "Question:",
+                                        style: TextStyle(
+                                          fontSize: 20,
+                                          color: Colors.grey[300],
+                                        ),
+                                      ),
+                                      ),
+                                    ),
+                                    Center(child: Text(request.question),),
+                                  ],
+                                ),
+                                textColor: Colors.white,
+                                onPressed: () {
+                                  // Navigator.pop(context);
+                                },
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 3,
+                            child: SizedBox.expand(
+                              child: RaisedButton(
+                                color: Universals.transparentColorWhite,
+                                child:
+                                StreamBuilder<DateTime>(
+                                  stream: Stream.periodic(const Duration(seconds: 1)),
+                                  builder: (context, snapshot) {
+                                    return
+                                      Center(
+                                        child:
+                                        Text(
+                                          "Time Waited: " +
+                                              DateTime.now().add(Duration(hours: -10)).difference(DateTime.parse(request.requestedTimeHawaii)).toString().substring(0, 7),
+                                        ),
+                                      );
+                                  },
+                                ),
+                                textColor: Colors.white,
+                                onPressed: () {
+                                },
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 3,
+                            child: SizedBox.expand(
+                              child: RaisedButton(
+                                color: Universals.buttonColor,
+                                child: Text(
+                                  "OK",
+                                  style: TextStyle(fontSize: 40),
+                                ),
+                                textColor: Colors.white,
+                                onPressed: () {
+                                  // print("done helping");
+                                  Navigator.pop(context);
+                                },
+                              ),
+                            ),
+                          ),
+                        ],
+                      );
+                }
+
+
+
+                return requestInfo;
+
+              }
+          ),
+
+
+        );
+      },
+    );
+
   }
 
 
